9deb62bcc8387dd9692b88e955141591928453fd
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-12-15 19:36:54 +0100

    feat: Create database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Greeting.create(greeting: 'Hey')
+Greeting.create(greeting: 'Hi')
+Greeting.create(greeting: 'Hello')
+Greeting.create(greeting: 'Bye')
+Greeting.create(greeting: 'Goodluck')
